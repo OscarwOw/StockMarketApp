0740901dbea6bb1c1dd1132fd210632da7038171
OscarwOw <OscarwOw210@gmail.com>
2023-09-12 21:13:20 +0200

    CardList and props validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Card from './Components/Card';
+import CardList from './Components/CardList';
 
 function App() {
   return (
     <div className="App">
-      <Card/>
-      <Card/>
+      <CardList/>
     </div>
   );
 }
--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
-type Props = {}
+interface Props {
+  companyName: string;
+  ticker: string;
+  price: number;
+}
 
-const Card = (props: Props) => {
+const Card: React.FC<Props> = ({companyName, ticker, price}: Props) : JSX.Element => {
   return (
     <div className='card'>Card
         <img src="https://www.savour.cz/img/post/9-5d2390fe0421b.jpg" alt="Image" />
     
     <div className='details'>
-        <h2>AAPL</h2>
-        <p>$110</p>
-
+        <h2>{companyName}({ticker})</h2>
+        <p>{price}</p>
+ 
     </div>
     <p className='infon'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, ad beatae quaerat assumenda fugit eligendi at et quasi blanditiis est reiciendis corrupti eos natus eius sed suscipit tempora, consequuntur necessitatibus.</p>
     </div>
